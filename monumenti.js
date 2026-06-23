d099e3fb8ec07a1a9a0b0c09845d0d7a3194e90f
f-niko <137630693+f-niko@users.noreply.github.com>
2026-06-23 12:45:24 +0200

    Update monumenti.js

--- monumenti.js
+++ monumenti.js
@@ -3,8 +3,8 @@ const monumenti = [
     {
         "id": 1,
         "nome": "Supermercato Esselunga di Via Milano",
-        "lat": 45.5415,
-        "lng": 10.2012,
+        "lat": 45.5446,
+        "lng": 10.2008,
         "desc": "Uno dei principali punti di rifornimento per il quartiere di Via Milano. Ottimo per fare scorta di pozioni e provviste prima di partire per l'esplorazione!",
         "categoria": "supermercato",
         "scoperto": false
@@ -13,7 +13,7 @@ const monumenti = [
         "id": 2,
         "nome": "Cimitero Monumentale di Brescia (Vantini)",
         "lat": 45.5408,
-        "lng": 10.2075,
+        "lng": 10.2021,
         "desc": "Il primo cimitero monumentale d'Italia, progettato dall'architetto Rodolfo Vantini. Un luogo ricco di sculture imponenti, architettura neoclassica e un'atmosfera solenne piena di storia.",
         "categoria": "monumento",
         "scoperto": false
@@ -21,8 +21,8 @@ const monumenti = [
     {
         "id": 3,
         "nome": "Centro Commerciale Freccia Rossa",
-        "lat": 45.5365,
-        "lng": 10.2135,
+        "lat": 45.5357,
+        "lng": 10.2084,
         "desc": "Situato nelle immediate vicinanze della stazione e della fine di Via Milano, un hub urbano perfetto per sbloccare obiettivi di orientamento e incontrare altri giocatori.",
         "categoria": "monumento",
         "scoperto": false
@@ -30,32 +30,23 @@ const monumenti = [
     {
         "id": 4,
         "nome": "Parco dell'Acqua",
-        "lat": 45.5348,
-        "lng": 10.2312,
+        "lat": 45.5340,
+        "lng": 10.2322,
         "desc": "Un'oasi verde urbana con laghetti e installazioni scientifiche dedicata alla risorsa più preziosa. Perfetto per una missione naturalistica.",
         "categoria": "parco",
         "scoperto": false
     },
     {
         "id": 5,
-        "nome": "Supermercato Coop (Brescia Via Milano)",
-        "lat": 45.5432,
-        "lng": 10.1945,
-        "desc": "Un punto di riferimento fondamentale per la spesa quotidiana lungo l'asse stradale. Fermati qui per completare la tua wishlist della spesa e guadagnare punti extra!",
-        "categoria": "supermercato",
-        "scoperto": false
-    },
-    {
-        "id": 6,
         "nome": "Parco Campo Marte",
-        "lat": 45.546,
-        "lng": 10.2185,
+        "lat": 45.5479,
+        "lng": 10.2162,
         "desc": "Un grande spazio verde cittadino non lontano da Via Milano, ideale per lo sport all'aperto, il relax e per cercare badge nascosti tra i suoi sentieri alberati.",
         "categoria": "parco",
         "scoperto": false
     },
     {
-        "id": 7,
+        "id": 6,
         "nome": "Prix di Via Milano",
         "lat": 45.5481,
         "lng": 10.1898,
